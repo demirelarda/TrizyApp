6a3fe27245b764f8dae62811c00a97f48cb3e735
arda <yademirel21@gmail.com>
2024-11-21 01:35:28 +0300

    Added networking manager

---
+++ lib/utils/api_endpoints.dart
@@ -0,0 +1,13 @@
+class ApiEndpoints {
+  static const String baseDevAndroidUrl = 'https://quickinbackend-production.up.railway.app';
+  static const String baseDeviOSUrl = 'https://quickinbackend-production.up.railway.app';
+  static const String baseDevWebUrl = 'https://quickinbackend-production.up.railway.app';
+  static const String baseProdUrl = 'https://quickinbackend-production.up.railway.app';
+
+  // AUTH
+  static const String register = 'api/register';
+  static const String login = 'api/login';
+  static const String refreshAccessToken = 'api/refresh';
+  static const String checkTokens = 'api/check-tokens';
+
+}
