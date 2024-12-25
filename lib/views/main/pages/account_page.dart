d3fa53d6111eaf6eecfaf5d769b57158dcf5dfd6
arda <yademirel21@gmail.com>
2024-12-26 01:08:05 +0300

    Added account page

--- lib/views/main/pages/account_page.dart
+++ lib/views/main/pages/account_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:trizy_app/theme/colors.dart';
+import '../../../components/basic_list_item.dart';
 
 class AccountPage extends StatefulWidget {
   const AccountPage({super.key});
@@ -12,19 +14,59 @@ class _AccountPageState extends State<AccountPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          const Spacer(),
-          Center(
-            child: TextButton(
-                onPressed: (){
-                  context.pushNamed("myAddresses");
-                },
-                child: const Text("Addresses")
+      backgroundColor: white,
+      appBar: AppBar(
+        title: const Text("My Account"),
+        centerTitle: true,
+        backgroundColor: white,
+        foregroundColor: Colors.black,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            BasicListItem(
+              text: "Profile",
+              onTap: () {
+                context.pushNamed('profile');
+              },
             ),
-          ),
-          const Spacer()
-        ],
+            BasicListItem(
+              text: "Addresses",
+              onTap: () {
+                context.pushNamed('myAddresses');
+              },
+            ),
+            BasicListItem(
+              text: "My Orders",
+              onTap: () {
+                context.pushNamed(
+                    'myOrders',
+                  pathParameters: {
+                    'fromAccount': "1",
+                  },
+                );
+              },
+            ),
+            BasicListItem(
+              text: "My Subscription",
+              onTap: () {
+                context.pushNamed('mySubscription');
+              },
+            ),
+            BasicListItem(
+              text: "Discount Codes",
+              onTap: () {
+                context.pushNamed('discountCodes');
+              },
+            ),
+            BasicListItem(
+              text: "Sign Out",
+              onTap: () {
+                context.goNamed("login");
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
